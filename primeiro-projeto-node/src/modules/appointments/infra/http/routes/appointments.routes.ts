27999f7321da4d41d90cacd69553f354417dce2a
lopessoftec <lopessoftec@gmail.com>
2020-12-16 17:55:20 -0300

    usando controllers

--- primeiro-projeto-node/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ primeiro-projeto-node/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,12 +1,10 @@
 import { Router } from 'express';
-import { parseISO } from 'date-fns';
-import {container } from 'tsyringe';
-
-import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+import AppointmentsController from '../controlers/AppointmentController';
 
 const appointmentsRouter = Router();
+const appointmentsController = new AppointmentsController();
 
 //para apçicar o midleware em todas as rotas de agentamentos
 appointmentsRouter.use(ensureAuthenticated);
@@ -14,26 +12,7 @@ appointmentsRouter.use(ensureAuthenticated);
 //SoC: Separation of Concerns (Separação de preocupações)
 // DTO - data transform object
 
-// appointmentsRouter.get('/', async (request, response) => {
-//   const appointments = await appointmentsRepository.find();
-
-//   return response.json(appointments);
-// });
-
 // http://localhost:3333/appointments
-appointmentsRouter.post('/', async (request, response) => {
-  const { provider_id, date } = request.body;
-
-  const parsedDate = parseISO(date);
-
-  const createAppointment = container.resolve(CreateAppointmentService);
-
-  const appointment = await createAppointment.execute({
-    date: parsedDate,
-    provider_id,
-  });
-
-  return response.json(appointment);
-});
+appointmentsRouter.post('/', appointmentsController.create);
 
 export default appointmentsRouter;
