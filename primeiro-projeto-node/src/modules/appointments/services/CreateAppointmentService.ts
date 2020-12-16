7885d0ace76ef5b84b3ccde03186696470292e31
lopessoftec <lopessoftec@gmail.com>
2020-12-16 01:05:16 -0300

    injeção de dependencias

--- primeiro-projeto-node/src/modules/appointments/services/CreateAppointmentService.ts
+++ primeiro-projeto-node/src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,4 +1,5 @@
 import { startOfHour } from 'date-fns';
+import {injectable, inject} from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
 
@@ -21,8 +22,12 @@ interface IRequest {
  * Dependency Inversion (SOLID)
  */
 
+ @injectable()
 class CreateAppointmentService {
-  constructor(private appointmentsRepository:IAppointmentsRepository){}
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository:IAppointmentsRepository
+    ){}
 
   public async execute({ date, provider_id }: IRequest): Promise<Appointment> {
 
