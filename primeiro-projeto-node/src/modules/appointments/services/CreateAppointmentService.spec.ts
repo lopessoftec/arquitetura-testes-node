7a0232f6402dcfb32b8825dd7a923e6138b9151b
lopessoftec <lopessoftec@gmail.com>
2020-12-16 21:19:22 -0300

    pensando nos testes

--- primeiro-projeto-node/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ primeiro-projeto-node/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,11 @@
-test('sum two numbers', () => {
-  expect(1 + 2).toBe(3);
-})
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('shold be able to create a new appointment', () => {
+    expect(1 + 2).toBe(3);
+  });
+
+  it('shold be able to create two appointment on the same time', () => {
+    expect(1 + 2).toBe(3);
+  });
+});
