8e1f84f5d2a4f326b3043dc3aab8a3c89c38acd4
lopessoftec <lopessoftec@gmail.com>
2020-12-28 20:03:37 -0300

    testando a autenticação, testes de autenticação

--- primeiro-projeto-node/src/modules/users/services/AuthenticateUserService.ts
+++ primeiro-projeto-node/src/modules/users/services/AuthenticateUserService.ts
@@ -1,10 +1,10 @@
-import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 import authConfig from '@config/auth';
 import { injectable, inject } from 'tsyringe';
 
-import IUsersRepository from '../repositories/IUsersRepository';
 import AppError from '@shared/errors/AppError';
+import IUsersRepository from '../repositories/IUsersRepository';
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
 
 import User from '../infra/typeorm/entities/User';
 
@@ -22,7 +22,10 @@ interface IResponse {
 class AuthenticateUserService {
   constructor(
     @inject('UsersRepository')
-    private usersRepository:IUsersRepository
+    private usersRepository:IUsersRepository,
+
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
     ){}
 
   public async execute({ email, password }: IRequest): Promise<IResponse> {
@@ -36,7 +39,7 @@ class AuthenticateUserService {
     // user.password - Senha criptografada
     // password - senha não-criptografada
 
-    const passwordMatched = await compare(password, user.password);
+    const passwordMatched = await this.hashProvider.compareHash(password, user.password);
 
     if (!passwordMatched) {
       throw new AppError('Incorrect email/password combination.', 401);
