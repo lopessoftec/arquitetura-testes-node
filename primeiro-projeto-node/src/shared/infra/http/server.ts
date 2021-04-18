3b7276d6d757668edd1bc9765bcd764241a843a6
lopessoftec <lopessoftec@gmail.com>
2021-04-18 15:40:14 -0300

    reset de senha

--- primeiro-projeto-node/src/shared/infra/http/server.ts
+++ primeiro-projeto-node/src/shared/infra/http/server.ts
@@ -17,7 +17,7 @@ const app = express();
 // cors evita que alguns sites que não são confiaveis para nossa aplicação possa acessar aqui
 app.use(cors());
 app.use(express.json());
-app.use('/files', express.static(uploadConfig.directory));
+app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
